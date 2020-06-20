72da76c50c568af1ce1eb27ae2d5d59332809aca
Francisco Olmos <francisco.olmos@unxdigital.com>
2020-06-20 15:08:41 -0300

    agregan funcionalidad

--- controller/movies.js
+++ controller/movies.js
@@ -1,18 +1,19 @@
-let db = require('../database/models')
+let db = require('../database/models');
+let moment = require('moment');
 let sequelize = db.sequelize;
 
 module.exports = movieController = {
-     list:  (req, res, next) =>  {
+    list: (req, res, next) => {
         db.Peliculas.findAll()
-        .then(peliculas => {
-            res.render('movies', {title: "Movies", movies: peliculas})
-        })
+            .then(peliculas => {
+                res.render('movies', { title: "Movies", movies: peliculas })
+            })
     },
     detail: (req, res, next) => {
         db.Peliculas.findByPk(req.params.id)
-        .then(pelicula => {
-            res.render('movieDetail', {title: pelicula.title + " detail", movie: pelicula})
-        })
+            .then(pelicula => {
+                res.render('movieDetail', { title: pelicula.title + " detail", movie: pelicula })
+            })
     },
     news: (req, res, next) => {
         db.Peliculas.findAll({
@@ -20,19 +21,32 @@ module.exports = movieController = {
                 ['release_date', 'DESC']
             ], limit: 5
         })
-        .then(newMovies => {
-            res.render('news', {title: 'New Movies', newMovies: newMovies})
-        })
+            .then(newMovies => {
+                res.render('news', { title: 'New Movies', newMovies: newMovies })
+            })
     },
     recommended: (req, res, next) => {
         db.Peliculas.findAll({
-        order: [
-            ['awards', 'DESC']
-        ], limit: 5
-    })
-    .then(recommended => {
-        res.render('recommended', {title: 'Top 5', recommendedMovies: recommended})
-    })
+            order: [
+                ['awards', 'DESC']
+            ], limit: 5
+        })
+            .then(recommended => {
+                res.render('recommended', { title: 'Top 5', recommendedMovies: recommended })
+            })
+    },
+    createForm: (req, res) => {
+        res.render('create')
+    },
+    createMovies: (req, res) => {
+        db.Peliculas.create({
+            title: req.body.title,
+            awards: req.body.awards,
+            length: req.body.length,
+            release_date: req.body.release_date
+        })
+        res.redirect('/movies');
+
     }
 
 }
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,8 @@ router.get('/movies', moviesController.list)
 router.get('/movies/detail/:id', moviesController.detail)
 router.get('/movies/new', moviesController.news)
 router.get('/movies/recommended', moviesController.recommended)
+router.get('/movies/create', moviesController.createForm)
+router.post('/movies/create/:id', moviesController.createMovies)
 
 
 module.exports = router;
